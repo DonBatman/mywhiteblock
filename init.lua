e3a8f9b1f111e457dc712a0c0153c82b83cb40ee
Don <serfdon@gmail.com>
2025-11-30 18:46:41 -0800

    You can use paint on fences, walls and stairs now

--- init.lua
+++ init.lua
@@ -1,7 +1,18 @@
+mywhiteblock = {}
 
+local stairs = core.settings:get_bool("mywhiteblock.stairs", true)
+local fences = core.settings:get_bool("mywhiteblock.fences", true)
+local walls = core.settings:get_bool("mywhiteblock.walls", true)
+if fences then
 dofile(minetest.get_modpath("mywhiteblock").."/fence.lua")
+end
+if stairs then
 dofile(minetest.get_modpath("mywhiteblock").."/stairs.lua")
+end
+if walls then
 dofile(minetest.get_modpath("mywhiteblock").."/walls.lua")
+end
+
 
 core.register_node("mywhiteblock:block", {
 	description = "White Block",
@@ -30,6 +41,42 @@ mywhiteblock.colors = {
 	{"yellow",     "Yellow",     "#e3ff00"},
 }
 
+if core.get_modpath("mydye") then
+	mywhiteblock.colors = {
+	{"black",      	"Black",      		"#000000"},
+	{"blue",       	"Blue",       		"#2000c9"},
+	{"brown",     	"Brown",      		"#954c05"},
+	{"cyan",      	"Cyan",       		"#01ffd8"},
+	{"darkgreen", 	"Dark Green",  		"#005b07"},
+	{"darkgrey",  	"Dark Grey",   		"#303030"},
+	{"green",     	"Green",      		"#61ff01"},
+	{"grey",       	"Grey",       		"#5b5b5b"},
+	{"magenta",    	"Magenta",    		"#ff05bb"},
+	{"orange",     	"Orange",     		"#ff8401"},
+	{"pink",      	"Pink",       		"#ff65b5"},
+	{"red",        	"Red",        		"#ff0000"},
+	{"violet",     	"Violet",     		"#ab23b0"},
+	{"white",      	"White",      		"#ffffff"},
+	{"yellow",     	"Yellow",     		"#e3ff00"},
+	{"peachpuff",	"Peachpuff", 		"#FFDAB9"},
+	{"navy",		"Navy", 			"#000080"},
+	{"coral",		"Coral", 			"#FF7F50"},
+	{"khaki",		"Khaki", 			"#F0E68C"},
+	{"lime",		"Lime", 			"#00FF00"},
+	{"light_pink",	"Light Pink", 		"#FFB6C1"},
+	{"light_grey",	"Light Grey", 		"#D3D3D3"},
+	{"purple",		"Purple", 			"#800080"},
+	{"maroon",		"Maroon", 			"#800000"},
+	{"aquamarine",	"Aqua Marine", 		"#7FFFD4"},
+	{"chocolate",	"Chocolate", 		"#D2691E"},
+	{"crimson",		"Crimson", 			"#DC143C"},
+	{"olive",		"Olive", 			"#808000"},
+	{"white_smoke",	"White Smoke", 		"#F5F5F5"},
+	{"mistyrose",	"Misty Rose", 		"#FFE4E1"},
+	{"orchid",		"Orchid", 			"#DA70D6"},
+	}
+end
+
 local paintables = {
 	"mywhiteblock:block"
 }
@@ -38,7 +85,6 @@ for _, entry in ipairs(mywhiteblock.colors) do
 	local color = entry[1]
 	local desc = entry[2]
 	local paint = "^[colorize:"..entry[3]
-
 core.register_node("mywhiteblock:block_" .. color, {
 	description = desc .. " Block",
 	tiles = {"mywhiteblock_white.png".. paint},
--- stairs.lua
+++ stairs.lua
@@ -1,21 +1,59 @@
 local block_table = {
-	{"black",      "Black",      "mywhiteblock_white.png^[colorize:#000000"},
-	{"blue",       "Blue",       "mywhiteblock_white.png^[colorize:#2000c9"},
-	{"brown",      "Brown",      "mywhiteblock_white.png^[colorize:#954c05"},
-	{"cyan",       "Cyan",       "mywhiteblock_white.png^[colorize:#01ffd8"},
-	{"darkgreen", "Dark Green",  "mywhiteblock_white.png^[colorize:#005b07"},
-	{"darkgrey",  "Dark Grey",   "mywhiteblock_white.png^[colorize:#303030"},
-	{"green",      "Green",      "mywhiteblock_white.png^[colorize:#61ff01"},
-	{"grey",       "Grey",       "mywhiteblock_white.png^[colorize:#5b5b5b"},
-	{"magenta",    "Magenta",    "mywhiteblock_white.png^[colorize:#ff05bb"},
-	{"orange",     "Orange",     "mywhiteblock_white.png^[colorize:#ff8401"},
-	{"pink",       "Pink",       "mywhiteblock_white.png^[colorize:#ff65b5"},
-	{"red",        "Red",        "mywhiteblock_white.png^[colorize:#ff0000"},
-	{"violet",     "Violet",     "mywhiteblock_white.png^[colorize:#ab23b0"},
-	{"white",      "White",      "mywhiteblock_white.png^[colorize:#ffffff"},
-	{"yellow",     "Yellow",     "mywhiteblock_white.png^[colorize:#e3ff00"},
+	{"black",      "Black",      "#000000"},
+	{"blue",       "Blue",       "#2000c9"},
+	{"brown",      "Brown",      "#954c05"},
+	{"cyan",       "Cyan",       "#01ffd8"},
+	{"darkgreen", "Dark Green",  "#005b07"},
+	{"darkgrey",  "Dark Grey",   "#303030"},
+	{"green",      "Green",      "#61ff01"},
+	{"grey",       "Grey",       "#5b5b5b"},
+	{"magenta",    "Magenta",    "#ff05bb"},
+	{"orange",     "Orange",     "#ff8401"},
+	{"pink",       "Pink",       "#ff65b5"},
+	{"red",        "Red",        "#ff0000"},
+	{"violet",     "Violet",     "#ab23b0"},
+	{"white",      "White",      "#ffffff"},
+	{"yellow",     "Yellow",     "#e3ff00"},
 }
-
+		
+if core.get_modpath("mydye") then
+	block_table = {
+	{"black",      	"Black",      		"#000000"},
+	{"blue",       	"Blue",       		"#2000c9"},
+	{"brown",     	"Brown",      		"#954c05"},
+	{"cyan",      	"Cyan",       		"#01ffd8"},
+	{"darkgreen", 	"Dark Green",  		"#005b07"},
+	{"darkgrey",  	"Dark Grey",   		"#303030"},
+	{"green",     	"Green",      		"#61ff01"},
+	{"grey",       	"Grey",       		"#5b5b5b"},
+	{"magenta",    	"Magenta",    		"#ff05bb"},
+	{"orange",     	"Orange",     		"#ff8401"},
+	{"pink",      	"Pink",       		"#ff65b5"},
+	{"red",        	"Red",        		"#ff0000"},
+	{"violet",     	"Violet",     		"#ab23b0"},
+	{"white",      	"White",      		"#ffffff"},
+	{"yellow",     	"Yellow",     		"#e3ff00"},
+	{"peachpuff",	"Peachpuff", 		"#FFDAB9"},
+	{"navy",		"Navy", 			"#000080"},
+	{"coral",		"Coral", 			"#FF7F50"},
+	{"khaki",		"Khaki", 			"#F0E68C"},
+	{"lime",		"Lime", 			"#00FF00"},
+	{"light_pink",	"Light Pink", 		"#FFB6C1"},
+	{"light_grey",	"Light Grey", 		"#D3D3D3"},
+	{"purple",		"Purple", 			"#800080"},
+	{"maroon",		"Maroon", 			"#800000"},
+	{"aquamarine",	"Aqua Marine", 		"#7FFFD4"},
+	{"chocolate",	"Chocolate", 		"#D2691E"},
+	{"crimson",		"Crimson", 			"#DC143C"},
+	{"olive",		"Olive", 			"#808000"},
+	{"white_smoke",	"White Smoke", 		"#F5F5F5"},
+	{"mistyrose",	"Misty Rose", 		"#FFE4E1"},
+	{"orchid",		"Orchid", 			"#DA70D6"},
+	}
+end
+local paintables = {
+	"stairs:slab_mywhiteblock:block"
+}
 
 
 for i in ipairs(block_table) do
@@ -28,11 +66,11 @@ for i in ipairs(block_table) do
 stairs.register_stair_and_slab("mywhiteblock:block_"..mat,
 			"mywhiteblock:block_"..mat, 
 			{cracky=2}, 
-			{img},
+			{"mywhiteblock_white.png^[colorize:"..img},
 			des.." Stairs", 
 			des.." Slab", 
 			default.node_sound_wood_defaults(), 
-			img,
+			"mywhiteblock_white.png^[colorize:"..img,
 			des.." Inner Stairs", 
 			des.." Outer Stairs")
 end
@@ -40,3 +78,11 @@ end
 stairs.register_stair_and_slab("mywhiteblock:block", "mywhiteblock:block", {cracky=2}, {"mywhiteblock_white.png"},
 		"White Stairs", "White Slab", default.node_sound_wood_defaults(), "mywhiteblock_white.png",
 		"White Inner Stairs", "White Outer Stairs")
+
+if minetest.get_modpath("mypaint") then
+local colors = {}
+for _, entry in ipairs(block_table) do
+	table.insert(colors, entry[1])
+end
+	mypaint.register(paintables, colors)
+end
